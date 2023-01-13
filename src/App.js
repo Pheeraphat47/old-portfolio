a2d8058a07a309daeb7d728994968541ed22d92c
P <dpheeraphat2547@gmail.com>
2023-01-13 16:39:49 +0700

    react icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-// import Footer from './components/footer';
+import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+
 
 
 
 function App() {
   return (
-    <div className="App">      
-       
-    </div>
+      <React.Fragment>
+          <Navbar/>
+      </React.Fragment>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,1 +1,25 @@
+import { FaBars, FaTimes } from "react-icons/fa";
+function Navbar() {
+    return (
+        <header>
+            <h3>Pheeraphat Dherachaisuphakij</h3>
+            <nav>
+                <a href="/#">Home</a>
+                <a href="/#">Award</a>
+                <a href="/#">Project</a>
+                <a href="/#">Activity</a>
+                <a href="/#">Contact</a>
+                {/* Close Button */}
+                <button>
+                    <FaTimes />
+                </button>
+            </nav>
+            {/* Hamberger Menu */}
+            <button>
+                <FaBars />
+            </button>
+        </header>
+    );
+}
 
+export default Navbar;
