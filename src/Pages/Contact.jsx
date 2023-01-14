d913dd6f4ad20f1b03b10012915b682230317b4e
P <dpheeraphat2547@gmail.com>
2023-01-14 10:45:42 +0700

    finish contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,10 @@
 function Contact(){
     return (
-        <div>
-            <h3>This is ContactPage</h3>
+        <div className="mt-60 flex flex-col content-center mx-auto space-y-32 w-32 h-32 ">            
+            <a href="https://github.com/Pheeraphat47" target={"_blank"}><img src="../assets/github.png" alt="github" className="hover:scale-150 duration-150 cursor-pointer"/></a>
+            <a href="https://www.linkedin.com/in/pheeraphat-dherachaisuphakij-33718b250/" target={"_blank"}><img src="../assets/linkedin.png" alt="linkedin" className="hover:scale-150 duration-150 cursor-pointer" /></a>
+            <a href="https://www.instagram.com/phx_pd/" target={"_blank"}><img src="../assets/instagram.png" alt="instagram" className="hover:scale-150 duration-150 cursor-pointer"/></a>
+            <a href="https://www.facebook.com/pheeraphat.dherachaisuphakij" target={"_blank"}><img src="../assets/facebook.png" alt="facebook" className="hover:scale-150 duration-150 cursor-pointer" /></a>            
         </div>
     )
 }
