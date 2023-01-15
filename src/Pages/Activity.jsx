8204c295bea184e1805640bf97784d5e7bf9f507
P <dpheeraphat2547@gmail.com>
2023-01-15 11:03:48 +0700

    finish all
    โครงคร่าวๆ

--- src/Pages/Activity.jsx
+++ src/Pages/Activity.jsx
@@ -1,9 +1,70 @@
-function Activity(){
-    return (
-        <div>
-            <h3>This is Activity Page</h3>
+function Activity() {
+  return (
+    <div>
+      <h1 className="text-5xl mt-32 text-center font-bold">Activity</h1>
+
+      {/* เรียงตามวันที่ได้  (ก่อน-หลัง)
+              1. Tobeit66
+              2. Charmcase Competition
+              3. Climathon
+              4. SIT Helloworld
+            */}
+
+      <div className="mt-40">
+        <div className="flex flex-row justify-center">
+          <div className="helloworld mr-40 shadow-2xl hover:scale-110 duration-150 ">
+            <img
+              src="../img/Energynex.jpg"
+              alt="helloworld"
+              className="w-80 h-96 object-cover"
+            />
+            <h3 className="text-center text-xl font-semibold pt-3 pb-2">
+              Young Innovators Bootcamp
+            </h3>
+            <p className="text-center text-lg">EnergyNex (Rank 4th from 10 teams)</p>
+          </div>
+
+          <div className="climathon shadow-2xl hover:scale-110 duration-150 ">
+            <img
+              src="../img/bettercountry.jpg"
+              alt="environhack"
+              className="w-80 h-96 object-cover"
+            />
+            <h3 className="text-center text-xl font-semibold pt-3 pb-2">
+              My Better Country Hackathon
+            </h3>
+            <p className="text-center text-lg">Konrad-Adenauer-Stiftung</p>
+          </div>
         </div>
-    )
+
+        <div className="flex flex-row justify-center mt-40">
+          <div className="charmcase mr-40 shadow-2xl hover:scale-110 duration-150 ">
+            <img
+              src="../img/42Bangkok.jpg"
+              alt="chula"
+              className="w-80 h-96 object-cover"
+            />
+            <h3 className="text-center text-xl font-semibold pt-3 pb-2">
+              International Startup Coding 
+            </h3>
+            <p className="text-center text-lg">42 Bangkok (Rank 19th from 26 teams)</p>
+          </div>
+
+          <div className="tobeit shadow-2xl hover:scale-110 duration-150">
+            <img
+              src="../img/Caihackathon.jpg"
+              alt="tobeit"
+              className="w-80 h-96 object-cover"
+            />
+            <h3 className="text-center text-2xl font-semibold pt-3 pb-2">
+              CAI CLUB HACKATHON
+            </h3>
+            <p className="text-center text-lg">CPALL</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Activity;
--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,9 +1,70 @@
-function Project(){
-    return (
-        <div>
-            <h3>This is ProjectPage</h3>
+function Project() {
+  return (
+    <div>
+      <h1 className="text-5xl mt-32 text-center font-bold">Projects</h1>
+
+      {/* เรียงตามวันที่ได้  (ก่อน-หลัง)
+              1. Tobeit66
+              2. Charmcase Competition
+              3. Climathon
+              4. SIT Helloworld
+            */}
+
+      <div className="mt-40">
+        <div className="flex flex-row justify-center">
+          <div className="worldcup mr-40 shadow-2xl hover:scale-110 duration-150 ">
+            <img
+              src="../img/worldcup.png"
+              alt="worldcup"
+              className="w-80 h-96 object-scale-down"
+            />
+            <h3 className="text-center text-2xl font-semibold pt-3 pb-2">
+              Worldcup Website
+            </h3>
+            <p className="text-center text-lg">Nodejs&Expressjs Projects</p>
+          </div>
+
+          <div className="registrationform shadow-2xl hover:scale-110 duration-150 ">
+            <img
+              src="../img/Registrationform.png"
+              alt="Registrationform"
+              className="w-80 h-96 object-scale-down"
+            />
+            <h3 className="text-center text-2xl font-semibold pt-3 pb-2">
+              Form Validation
+            </h3>
+            <p className="text-center text-lg">React Projects</p>
+          </div>
         </div>
-    )
+
+        <div className="flex flex-row justify-center mt-40">
+          <div className="diary mr-40 shadow-2xl hover:scale-110 duration-150 ">
+            <img
+              src="../img/Diary.png"
+              alt="Diary"
+              className="w-80 h-96 object-scale-down"
+            />
+            <h3 className="text-center text-2xl font-semibold pt-3 pb-2">
+              Diary
+            </h3>
+            <p className="text-center text-lg">Web Technology Projects</p>
+          </div>
+
+          <div className="todolist shadow-2xl hover:scale-110 duration-150">
+            <img
+              src="../img/todolist.png"
+              alt="todolist"
+              className="w-80 h-96 object-scale-down"
+            />
+            <h3 className="text-center text-2xl font-semibold pt-3 pb-2">
+              To-do-list
+            </h3>
+            <p className="text-center text-lg">TypeScript Projects</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
